b135d620e51205080f5f653972127011b289320a
roptzhp <hyzph@163.com>
2022-02-09 16:30:54 +0800

    第一个成功的配套的后端程序

---
+++ router/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const con = require('../db/db.js')
+
+const router = express.Router()
+
+let db= con.handleDisconnection()
+
+router.get('/getstaff', (req, res) => {
+    let sql = 'SELECT * FROM staff'
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send(results)
+      }
+    })
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const router = require('./router/index.js')
+const app = express()
+const path = require('path')
+
+// 跨域请求处理
+//静态资源托管后，就能访问网页
+//app.use(express.static(path.join(__dirname,'./dist')))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(router)
+
+app.listen(5000,()=>{
+    console.log('server is running at http://localhost:5000')
+})
